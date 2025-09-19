3ecc5ea4b6cb38117ec1c9a700634d8547eddd3b
vinaypall <vishalpal2367@gmail.com>
2025-09-19 17:16:27 +0530

    Connect backend and frontend

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -1,7 +1,7 @@
 import Job from "../models/job.js";
 
 // get all job
-export const getJob = async (req, res) => {
+export const getJobs = async (req, res) => {
   try {
     const jobs = await Job.find({ visible: true }).populate({
       path: "companyId",
@@ -20,4 +20,30 @@ export const getJob = async (req, res) => {
 };
 
 //get a single job by id
-export const getJobById = async (req, res) => {};
+// get a single job by id
+export const getJobById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const jobs = await Job.findById(id).populate({
+      path: "companyId",
+      select: "-password",
+    });
+
+    if (!jobs) {
+      return res.status(404).json({
+        success: false,
+        message: "Job not found",
+      });
+    }
+
+    res.json({
+      success: true,
+      jobs,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
