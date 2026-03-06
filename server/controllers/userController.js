b444d7de4e8254777efcfe48decdda95b7384a23
vinaypall <vishalpal2367@gmail.com>
2026-03-06 17:39:12 +0530

    update req

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,7 @@ import User from "../models/users.js";
 import { v2 as cloudinary } from "cloudinary";
 //Get user data
 export const getUserData = async (req, res) => {
-  const userId = req.auth.userId;
+  const userId = req.auth().userId;
   try {
     const user = await User.findById(userId);
     if (!user) {
@@ -28,7 +28,7 @@ export const getUserData = async (req, res) => {
 //Apply for a job
 export const applyForJob = async (req, res) => {
   const { jobId } = req.body;
-  const userId = req.auth.userId;
+  const userId = req.auth().userId;
   try {
     const isAlreadyApplied = await JobApplication.findOne({ jobId, userId });
     if (isAlreadyApplied) {
@@ -63,7 +63,7 @@ export const applyForJob = async (req, res) => {
 //Get User applied applicants
 export const getUserJobApplications = async (req, res) => {
   try {
-    const userId = req.auth.userId;
+    const userId = req.auth().userId;
     const applications = await JobApplication.find({ userId })
       .populate("companyId", "name email image")
       .populate("jobId", "title description location category level salary")
@@ -84,7 +84,7 @@ export const getUserJobApplications = async (req, res) => {
 //Update user profile
 export const updateUserResume = async (req, res) => {
   try {
-    const userId = req.auth.userId;
+    const userId = req.auth().userId;
     const resumeFile = req.file;
     const userData = await User.findById(userId);
     if (resumeFile) {
